be2fd74624d697e083cae2e3918f39258434ee73
Abby Jordan <abhaydeora97@gmail.com>
2024-07-20 18:08:38 +0530

    Added config file for accessing variables

--- src/components/BlogPostList/index.js
+++ src/components/BlogPostList/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { CircularProgress, Container, Grid, Pagination } from '@mui/material';
 import BlogPostItem from '../BlogPostItem';
 import axios from 'axios';
-
+import config from '../../config';
 
 const BlogPostList = () => {
   const [posts, setPosts] = useState([]);
@@ -10,39 +10,62 @@ const BlogPostList = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_NEWS_API_URL}/everything?q=blog&pageSize=10&page=${page}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`)
-    .then(response => {
-      setPosts(response.data.articles);
-      setLoading(false);
-    })
-      .catch(error => {console.error(error);setLoading(false);});
+    axios
+      .get(
+        `${config.API_URL}/everything?q=blog&pageSize=10&page=${page}&apiKey=${config.API_KEY}`
+      )
+      .then((response) => {
+        setPosts(response.data.articles);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
   }, [page]);
 
   return (
-    <Container sx={{ backgroundColor: '#f5f5f5', minHeight: '100vh', paddingTop: '20px' }}>
-    {loading ? (
-      <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
-        <CircularProgress />
-      </Grid>
-    ) : (
-      <>
-        <Grid container spacing={3}>
-          {posts.map((post, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-              <BlogPostItem post={post} />
-            </Grid>
-          ))}
+    <Container
+      sx={{
+        backgroundColor: '#f5f5f5',
+        minHeight: '100vh',
+        paddingTop: '20px',
+      }}
+    >
+      {loading ? (
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          style={{ minHeight: '100vh' }}
+        >
+          <CircularProgress />
         </Grid>
-        <Pagination
-          sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}
-          count={10}
-          page={page}
-          onChange={(event, value) => {setPage(value); window.scrollTo({ top: 0, behavior: 'smooth' })}}
-        />
-      </>
-    )}
-  </Container>
-
+      ) : (
+        <>
+          <Grid container spacing={3}>
+            {posts.map((post, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <BlogPostItem post={post} />
+              </Grid>
+            ))}
+          </Grid>
+          <Pagination
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              marginTop: '20px',
+            }}
+            count={10}
+            page={page}
+            onChange={(event, value) => {
+              setPage(value);
+              window.scrollTo({ top: 0, behavior: 'smooth' });
+            }}
+          />
+        </>
+      )}
+    </Container>
   );
 };
 
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  API_URL: 'https://newsapi.org/v2',
+  API_KEY: '17cd0a932b214324b7ec186853e2befc',
+};
+
+export default config;
